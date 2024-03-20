cce7d4d6285b98331e4ccdf215ad4b1ab52c53f4
Tigran Dzhamalyan <tigr.sa2021@gmail.com>
2024-03-20 21:54:45 +0300

    add: added default layout and realized basic styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,70 +13,74 @@ export default {
     "./app.vue",
     "./error.vue",
   ],
-  theme: {
-    colors: {
-      primary,
-      black:colors.black,
-      white:colors.white,
-      transparent:colors.transparent,
-      yellow: {
-        700: '#F5C521'
-      },
-      gray: {
-        300: '#d9dae8',
-        500: '#999aa5',
-        600: '#66676a',
-      }
-    },
-    extend: {
-      spacing: {
-        0.5: '0.12rem',
-        layout: '2.75rem',
-      },
-      fontSize: {
-        '2lg': '1.38rem',
-      },
-      borderRadius: {
-        image: '0.5rem',
-        layout: '0.8rem',
-      },
-      transitionTimingFunction: {
-        DEFAULT: 'ease-in-out',
-      },
-      transitionDuration: {
-        DEFAULT: '0.2s',
-      },
-      zIndex: {
-        1: '1',
-        2: '2',
-        3: '3',
-      },
-      keyframes: {
-        fade: {
-          from: { opacity: 0 },
-          to: { opacity: 1 },
-        },
-        scaleIn: {
-          '0%': {
-            opacity: 0,
-            transform: 'scale(0.9)',
-          },
-          '50%': {
-            opacity: 0.3,
-          },
-          '100%': {
-            opacity: 1,
-            transform: 'scale(1)',
-          },
-        },
-        animation: {
-          fade: 'fade .5s ease-in-out',
-          scaleIn: 'scaleIn .35s ease-in-out',
+	theme: {
+		colors: {
+			primary,
+			black: colors.black,
+			white: colors.white,
+			gray: {
+				300: '#d9dae8',
+				500: '#999AA5',
+				600: '#66676E',
+				700: '#39393f',
+				800: '#242529',
+				900: '#191B1F',
+				950: '#101215',
+			},
+			transparent: colors.transparent,
+			yellow: {
+				700: '#F5C521',
+			},
+		},
 
-        }
-      },
-    },
-  },
+		extend: {
+			spacing: {
+				0.5: '0.12rem',
+				layout: '2.75rem',
+			},
+			fontSize: {
+				'2lg': '1.38rem',
+			},
+			borderRadius: {
+				image: '0.5rem',
+				layout: '0.8rem',
+			},
+			transitionTimingFunction: {
+				DEFAULT: 'ease-in-out',
+			},
+			transitionDuration: {
+				DEFAULT: '200ms',
+			},
+			keyframes: {
+				fade: {
+					from: { opacity: 0 },
+					to: { opacity: 1 },
+				},
+				scaleIn: {
+					'0%': {
+						opacity: 0,
+						transform: 'scale(0.9)',
+					},
+					'50%': {
+						opacity: 0.3,
+					},
+					'100%': {
+						opacity: 1,
+						transform: 'scale(1)',
+					},
+				},
+			},
+			animation: {
+				fade: 'fade .5s ease-in-out',
+				scaleIn: 'scaleIn .35s ease-in-out',
+			},
+			zIndex: {
+				1: '1',
+				2: '2',
+				3: '3',
+			},
+		},
+	},
   plugins: [
 
   ],
