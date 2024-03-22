9d55a97228462cd98190c93d92764a94c0ca6cad
Tigran Dzhamalyan <tigr.sa2021@gmail.com>
2024-03-22 18:16:49 +0300

    add: added uiSearchField

---
+++ components/ui/searchField.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+defineProps<{
+  modelValue: string;
+}>();
+
+const emit = defineEmits(["update:modelValue"]);
+
+const handleSearch = (event: Event) => {
+  const inputValue = (event.target as HTMLInputElement).value;
+  emit('update:modelValue', inputValue);
+};
+</script>
+
+<template>
+  <div class="search">
+    <MaterialIcon name="MdSearch" />
+    <input 
+      placeholder="Search" 
+      :value="modelValue" 
+      @input=handleSearch 
+    />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.search {
+	@apply rounded-3xl border border-gray-700 px-3 py-2 flex items-center transition-colors;
+
+	> svg {
+		@apply fill-gray-600 mr-2 text-3xl transition-colors;
+	}
+
+	> input {
+		@apply text-white placeholder:text-gray-600 block w-full bg-transparent outline-none;
+	}
+
+	&:focus-within {
+		@apply border-gray-600;
+		> svg {
+			@apply fill-gray-500;
+		}
+	}
+}
+
+</style>
