d0cd8507f0883ea754bac79afbc39191fe9ecbbe
Tigran Dzhamalyan <tigr.sa2021@gmail.com>
2024-03-22 12:36:55 +0300

    add: init seo optimization

--- app.vue
+++ app.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Online cinema',
+	ogTitle: 'Online cinema',
+	description: 'This is my amazing site, let me tell you all about it.',
+	ogDescription: 'This is my amazing site, let me tell you all about it.',
+});
+</script>
+
 <template>
 	<NuxtLayout>
 		<NuxtPage />
