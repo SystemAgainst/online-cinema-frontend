d6a115d6867c80368e70a5f3dac0304784fab71c
Tigran Dzhamalyan <tigr.sa2021@gmail.com>
2024-03-24 17:25:22 +0300

    add: added custom debounce function

---
+++ composables/useDebounce.js
@@ -0,0 +1,19 @@
+export const useDebouncedRef = (value, delay = 200) => {
+  let timeout;
+
+  return customRef((track, trigger) => {
+    return {
+      get() {
+        track()
+        return value
+      },
+      set(newValue) {
+        clearTimeout(timeout)
+        timeout = setTimeout(() => {
+          value = newValue
+          trigger()
+        }, delay)
+      }
+    };
+  });
+};
